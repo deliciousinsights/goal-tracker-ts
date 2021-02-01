d6e281e6581774f3dfa9cde463fa3fee7ed60552
Christophe Porteneuve <tdd@tddsworld.com>
2021-02-01 22:53:01 +0100

    Début exercice étape 9 : bascule sur store Redux, Provider et useSelector basiques

--- src/main/HomeScreen.tsx
+++ src/main/HomeScreen.tsx
@@ -1,9 +1,11 @@
 import LoginScreen from '../auth/LoginScreen'
-import store from '../store'
 import TrackerScreen from './TrackerScreen'
+import { useAppSelector } from '../store'
 
 export default function HomeScreen() {
-  const loggedIn = store.currentUser.loginState === 'logged-in'
+  const loggedIn = useAppSelector(
+    (state) => state.currentUser.loginState === 'logged-in'
+  )
 
   return loggedIn ? <TrackerScreen /> : <LoginScreen />
 }
