09406a36fe13cf6ec51e2c86db21c8349be39022
Christophe Porteneuve <tdd@tddsworld.com>
2021-02-01 23:10:18 +0100

    Début étape 15 : HistoryScreen

---
+++ src/history/HistoryDay.spec.tsx
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/react'
+
+import HistoryDay from './HistoryDay'
+import type { HistoryEntry } from '../reducers/history'
+
+describe('<HistoryDay />', () => {
+  const goals = [
+    {
+      id: '5bf57a79890a6e2c11ec9665',
+      name: 'Demo goal 1',
+      target: 5,
+      units: 'wombats',
+    },
+    {
+      id: '5bf57a79890a6e2c11ec9666',
+      name: 'Demo goal 2',
+      target: 10,
+      units: 'weazels',
+    },
+  ]
+
+  it('should otherwise match the expected snapshot', () => {
+    const stats = {
+      date: '2017-03-23',
+      progresses: { '0': [1, 5] },
+    } as HistoryEntry
+    const { container } = render(<HistoryDay goals={goals} stats={stats} />)
+
+    expect(container).toMatchSnapshot()
+  })
+})
--- src/history/HistoryScreen.tsx
+++ src/history/HistoryScreen.tsx
@@ -1,9 +1,46 @@
+import { Link } from 'react-router-dom'
 import { useEffect } from 'react'
+
+import ArrowBack from '@mui/icons-material/ArrowBack'
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardHeader from '@mui/material/CardHeader'
+import ClearIcon from '@mui/icons-material/Clear'
+import Typography from '@mui/material/Typography'
+
+import { clearHistory } from '../reducers/history'
+import HistoryDay from './HistoryDay'
+import { useAppDispatch, useAppSelector } from '../store'
 
 export default function HistoryScreen() {
   useEffect(() => {
     document.title = 'Mon historique'
   }, [])
 
-  return <h1>History coming soon</h1>
+  const { goals, history } = useAppSelector(({ goals, history }) => ({
+    goals,
+    history,
+  }))
+  const dispatch = useAppDispatch()
+
+  return (
+    <>
+      <Button component={Link} startIcon={<ArrowBack />} to='/' variant='text'>
+        Retour
+      </Button>
+      <Card className='history'>
+        <CardHeader title='Historique' />
+        <CardContent>
+          <Typography>Coming soon: history</Typography>
+        </CardContent>
+        <CardActions>
+          <Button startIcon={<ClearIcon />} variant='contained'>
+            Effacer
+          </Button>
+        </CardActions>
+      </Card>
+    </>
+  )
 }
