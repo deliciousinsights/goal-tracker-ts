163c2ebee34529d4888a9c1d4c02493b7266087f
Christophe Porteneuve <tdd@tddsworld.com>
2021-02-01 22:53:08 +0100

    Étape 10 : UX login interactif

--- src/auth/LoginScreen.tsx
+++ src/auth/LoginScreen.tsx
@@ -1,3 +1,6 @@
+import type { FormEvent } from 'react'
+import { useState } from 'react'
+
 import ArrowForward from '@mui/icons-material/ArrowForward'
 import Button from '@mui/material/Button'
 import Card from '@mui/material/Card'
@@ -7,11 +10,18 @@ import CardHeader from '@mui/material/CardHeader'
 import TextField from '@mui/material/TextField'
 
 import classes from './LoginScreen.module.css'
+import { logIn } from '../reducers/currentUser'
 import TogglablePasswordField from './TogglablePasswordField'
+import { useAppDispatch } from '../store'
 
 export default function LoginScreen() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const dispatch = useAppDispatch()
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <Card className={classes.loginScreen}>
         <CardHeader title='Goal Tracker' subheader='Connexion' />
         <CardContent>
@@ -22,9 +32,11 @@ export default function LoginScreen() {
             label='E-mail'
             fullWidth
             margin='normal'
+            onChange={(e) => setEmail(e.target.value.toLowerCase())}
             placeholder='mon@email.tld'
             required
             type='email'
+            value={email}
           />
           <TogglablePasswordField
             autoComplete='current-password'
@@ -33,8 +45,10 @@ export default function LoginScreen() {
             label='Mot de passe'
             fullWidth
             margin='normal'
+            onChange={(e) => setPassword(e.target.value)}
             placeholder='super mot de passe'
             required
+            value={password}
           />
         </CardContent>
         <CardActions style={{ display: 'flex', justifyContent: 'center' }}>
@@ -50,4 +64,9 @@ export default function LoginScreen() {
       </Card>
     </form>
   )
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+    dispatch(logIn({ email, password }))
+  }
 }
