d13e1addebfcc20b2486d2025015a69cfe544bea
Christophe Porteneuve <tdd@tddsworld.com>
2021-02-01 22:37:27 +0100

    Étape 3 : Gauge et base pour TrackerScreen

--- src/shared/Gauge.tsx
+++ src/shared/Gauge.tsx
@@ -1,5 +1,20 @@
 import LinearProgress from '@mui/material/LinearProgress'
 
-export default function Gauge() {
-  return <p>COMING SOON: GAUGE</p>
+interface GaugeProps {
+  max?: number
+  value: number
+}
+
+export default function Gauge({ max = 100, value }: GaugeProps) {
+  return (
+    <LinearProgress
+      style={{ height: 8 }}
+      variant='determinate'
+      value={normalize(value, max)}
+    />
+  )
+}
+
+function normalize(value: number, max: number) {
+  return Math.min(value * (100 / max), 100)
 }
