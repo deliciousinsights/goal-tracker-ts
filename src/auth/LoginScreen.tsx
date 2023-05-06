8409afb15ca83f142aeff62d88c7a941ad9fc113
Christophe Porteneuve <christophe@delicious-insights.com>
2023-05-06 16:46:56 +0100

    Début étape 13 : login asynchrone

--- src/auth/LoginScreen.tsx
+++ src/auth/LoginScreen.tsx
@@ -7,20 +7,32 @@ import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
 import CardHeader from '@mui/material/CardHeader'
+import Snackbar from '@mui/material/Snackbar'
 import TextField from '@mui/material/TextField'
 
 import classes from './LoginScreen.module.css'
 import { logIn } from '../reducers/currentUser'
 import TogglablePasswordField from './TogglablePasswordField'
-import { useAppDispatch } from '../store'
+import { useAppDispatch, useAppSelector } from '../store'
 
 const MIN_PASSWORD_LENGTH = 6
 
 export default function LoginScreen() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const canLogIn = email !== '' && password.trim().length >= MIN_PASSWORD_LENGTH
+  const loginState = useAppSelector((state) => state.currentUser.loginState)
   const dispatch = useAppDispatch()
+  const loggingIn = loginState === 'pending'
+  const logInIcon = loggingIn ? null : <ArrowForward />
+  const canLogIn =
+    !loggingIn && email !== '' && password.trim().length >= MIN_PASSWORD_LENGTH
+
+  const snackBar =
+    loginState === 'failure' ? (
+      <Snackbar message='Identifiant ou mot de passe invalide' open />
+    ) : (
+      ''
+    )
 
   return (
     <form onSubmit={handleSubmit}>
@@ -58,7 +70,7 @@ export default function LoginScreen() {
           <Button
             color='primary'
             disabled={!canLogIn}
-            startIcon={<ArrowForward />}
+            startIcon={logInIcon}
             type='submit'
             variant='contained'
           >
@@ -66,6 +78,7 @@ export default function LoginScreen() {
           </Button>
         </CardActions>
       </Card>
+      {snackBar}
     </form>
   )
 
